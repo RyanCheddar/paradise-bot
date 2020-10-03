e072405525a06c08da4c07925166908178281878
RyanC <44641166+RyanCheddar@users.noreply.github.com>
2020-10-03 22:51:54 +0800

    Update rewards.py
    fixed comment

--- rewards.py
+++ rewards.py
@@ -1,4 +1,4 @@
 import asyncio
 
 async def updatebalance(userid, action, baltype, amount):
-  // TODO - I have no fucking clue how Github works so this is just test
+  # TODO - I have no fucking clue how Github works so this is just test
