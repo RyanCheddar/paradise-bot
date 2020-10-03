9605b9a6f09f3daa169351c48904139f6797345c
RyanC <44641166+RyanCheddar@users.noreply.github.com>
2020-10-03 23:25:11 +0800

    Update balance.py
    poger graphics

--- rewards/balance.py
+++ rewards/balance.py
@@ -1,4 +1,17 @@
 import asyncio
 
+# ===================================================
+# || Update Balance - Updates Para Rewards balance ||
+# ||                                               ||
+# || userid - The user's id                        ||
+# ||                                               ||
+# || action - The action being done, valid options ||
+# || are "add" and "subtract"                      ||
+# ||                                               ||
+# || baltype - The balance type, can be "credits"  ||
+# || or "points"                                   ||
+# ||                                               ||
+# || amount - The amount being changed             ||
+# ===================================================
 async def updatebalance(userid, action, baltype, amount):
   # TODO - I have no fucking clue how Github works so this is just test
