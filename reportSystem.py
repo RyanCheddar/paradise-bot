6c317dc78ada942c13765ffbf06b272048c265a8
SkeldrBoii <72744476+SkeldrBoii@users.noreply.github.com>
2020-10-14 12:33:22 +0200

    Update reportSystem.py

--- reportSystem.py
+++ reportSystem.py
@@ -13,18 +13,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("p!report"):
-        reportcontent = message.content.split(None, 1)[1]
-        reportchannel = client.get_channel(765525235042222080)
-        reporter = message.author
-        successMsg = discord.Embed(color=0x00ff1a)
-        successMsg.add_field(name="Successfull", value="Report sumitted!", inline=False)
-        successMsg.set_footer(text=str(reporter))
-        await message.channel.send(embed=successMsg)
-        reportFinal = discord.Embed(title="Report by " + str(reporter), description=str(reportcontent), timestamp=datetime.datetime.now(datetime.timezone.utc), inline=False, color=0xff0000)
-        await reportchannel.send(embed=reportFinal)
-
-    if message.content == 'cheddar':
-        await message.channel.send(":PogChamp~5:")
+        try:
+            reportcontent = message.content.split(None, 1)[1]
+            reportchannel = client.get_channel(765525235042222080)
+            reporter = message.author
+            successMsg = discord.Embed(color=0x00ff1a)
+            successMsg.add_field(name="Successfull", value="Report submitted!", inline=False)
+            successMsg.set_footer(text=str(reporter))
+            await message.channel.send(embed=successMsg)
+            reportFinal = discord.Embed(title="Report by " + str(reporter), description=str(reportcontent), timestamp=datetime.datetime.now(datetime.timezone.utc), inline=False, color=0xff0000)
+            await reportchannel.send(embed=reportFinal)
+        except IndexError:
+            errorMsg = discord.Embed(name="Incorrect syntax", value="Usage: p!report [report]", inline=False, color=0xff0000)
+            await message.channel.send(embed=errorMsg)
 
 
 # CHANGED: prettier
